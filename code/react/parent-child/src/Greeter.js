e7528a1c7b77777311841e70be032ffc24d2f01c
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-12-17 16:50:28 +0100

    finisehd slides for anguar hh meetup 2017

--- code/react/parent-child/src/Greeter.js
+++ code/react/parent-child/src/Greeter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class HelloMessage extends React.Component {
+export default class Greeter extends React.Component {
     render() {
         return (
             <div>
